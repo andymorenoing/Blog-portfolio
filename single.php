0f8a2724e64103de9857555613429028060139d5
Andy <andymoreno.ing@gmail.com>
2018-04-22 10:15:19 -0400

    Trabajandno en el single

---
+++ single.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Andy | Portfolio</title>
+  <link rel="stylesheet" href="css/fuentes.css">
+	<link rel="stylesheet" href="css/font/fontawesome-all.css" >
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <header id="header" class="main-header main-header-blog">
+		<div class="sub-header">
+			<div class="logo-blog">
+				<h2 class="no-margin">{{ a<span>ndy</span> }}</h2>
+			</div>
+			<div class="search_header no-padding">
+				<input type="text" placeholder="¿Qué estás buscando?">
+			</div>
+			<div class="main-menu-blog">
+				<ul class="menu-ul">
+					<li><a href="#">Proyectos<span class="colorbar"></span></a></li>
+					<li><a href="#">Blog<span class="colorbar"></span></a></li>
+					<li><a href="#">Contacto<span class="colorbar"></span></a></li>
+				</ul>
+			</div>
+		</div>
+  </header>
+    <main>
+        <div class="cont-main">
+            <div class="content-single ed-item m-70">
+                <div class="header-single main-center cross-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="author-single ed-item main-center">
+                        <h1>Titulo del Articulo</h1>
+                        <p>Autor: Andy Moreno</p>
+                        <img src="img/Yo.jpg" alt="">
+                        <div class="author-social ed-container main-center">
+                            <a href="#" title="Sígueme en Facebook"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#" title="Sígueme en Twitter"><i class="fab fa-twitter"></i></a>
+                            <a href="#" title="Sígueme en Github"><i class="fab fa-github-alt"></i></a>
+                            <a href="#" title="Sígueme en Linkedin"><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="ed-item">
+                    <pre>&lt;?php hola ?&gt;</pre>
+                    <p>
+                        What is Lorem Ipsum?
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                        Why do we use it?
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+                        Where does it come from?
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    </p>
+                    <p>
+                        What is Lorem Ipsum?
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                        Why do we use it?
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+                        Where does it come from?
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    </p>
+                    <p>
+                        What is Lorem Ipsum?
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                        Why do we use it?
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+
+
+                        Where does it come from?
+                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+                        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    </p>
+                </div>
+            </div>
+            <div id="sidebar" class="ed-item m-30">
+                <div class="sidebar-single">
+                    <div class="ed-container">
+                        <div class="buttom-sidebar ed-item l-1-3">
+                            <button v-on:click="clickButtonRe" :valor="valorRe" :style="CssButton">Relacionados</button>
+                        </div>
+                        <div class="buttom-sidebar ed-item l-1-3">
+                            <button v-on:click="clickButtonTo" :valor="valorTo" :style="CssButtonTo">Art. top 6!</button>
+                        </div>
+                        <div class="buttom-sidebar ed-item l-1-3">
+                            <button v-on:click="clickButtonFo" :valor="valorFo" :style="CssButtonFo">Mensaje</button>
+                        </div>
+                       
+                       
+                        <div :class="formRe">
+                            <div class="articles-rating">
+                                <h3>Articulos Relacionados</h3>
+                                <div class="ed-item">
+                                    <?php 
+                                    for ($i = 0; $i< 6; $i++){
+                                    ?>
+                                    <div class="article-rating ed-container">
+                                        <div class="share" title="Compartir">
+                                            <i class="fas fa-share-square"></i>
+                                        </div>
+                                        <div class="ed-item l-40 images-rating cross-center main-center">
+                                            <img src="img/blog.jpg" alt="">
+                                            <div class="rating-star">
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                            </div>
+                                        </div>
+                                        <div class="ed-item l-60">
+                                            <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                                            <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                                            <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                                        </div>
+                                    </div>
+                                    <?php 
+                                    }
+                                    ?>
+                                </div>  
+                            </div>
+                        </div> 
+                        <div :class="formTo">
+                            <div class="articles-rating">
+                                <h3>Articulos top 6!</h3>
+                                <div class="ed-item">
+                                    <?php 
+                                    for ($i = 0; $i< 6; $i++){
+                                    ?>
+                                    <div class="article-rating ed-container">
+                                        <div class="share" title="Compartir">
+                                            <i class="fas fa-share-square"></i>
+                                        </div>
+                                        <div class="ed-item l-40 images-rating cross-center main-center">
+                                            <img src="img/blog.jpg" alt="">
+                                            <div class="rating-star">
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                                <i class="fas fa-star"></i>
+                                            </div>
+                                        </div>
+                                        <div class="ed-item l-60">
+                                            <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                                            <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                                            <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                                        </div>
+                                    </div>
+                                    <?php 
+                                    }
+                                    ?>
+                                </div>  
+                            </div>
+                        </div>  
+                        <div :class="formFo">
+                            <div class="articles-rating">
+                                <h3>Contáctate conmigo!</h3>
+                                <formvue></formvue>
+                            </div> 
+                        </div> 
+                    </div>  
+                </div>  
+            </div>
+        </div>
+	</main>
+	<footer class="main-footer-page">
+		<div class="footer-info">
+            <p>Ingeniero de Sistemas, Andy Moreno.</p>
+        </div>
+		<div class="footer-social">
+			<span><a href="#" title="Sígueme en Facebook"><i class="fab fa-facebook-f"></i></a></span>
+			<span><a href="#" title="Sígueme en Twitter"><i class="fab fa-twitter"></i></a></span>
+			<span><a href="#" title="Sígueme en Github"><i class="fab fa-github-alt"></i></a></span>
+			<span><a href="#" title="Sígueme en Linkedin"><i class="fab fa-linkedin-in"></i></a></span>
+		</div>
+		<div class="footer-date">
+            <p>11 Febrero 2018. Venezuela</p>
+        </div>
+	</footer>
+    <script src="js/vue.js"></script>
+    <script src="js/comp-form.js"></script>
+    <script>
+           
+    const mv = new Vue({
+        el: '#sidebar',
+        data: {
+            valorRe: true,
+            formRe: '',
+            CssButton: 'background: #008AE6; color: #fff',
+
+            valorTo: false,
+            formTo: 'oculto',
+            CssButtonTo: '',
+
+
+            valorFo: false,
+            formFo: 'oculto',
+            CssButtonFo: ''    
+        },
+        methods: {
+            clickButtonRe(){
+                if (this.valorRe == false){
+                    this.valorRe = true,
+                    this.formRe = '',
+                    this.CssButton = 'background: #008AE6; color: #fff',
+
+                    this.valorTo = false,
+                    this.formTo = 'oculto',
+                    this.CssButtonTo = '',
+
+                    this.valorFo = false,
+                    this.formFo = 'oculto',
+                    this.CssButtonFo = ''
+               }
+            },
+            clickButtonTo(){
+               if (this.valorTo == false){
+                    this.valorTo = true,
+                    this.formTo = '',
+                    this.CssButtonTo = 'background: #008AE6; color: #fff',
+
+                    this.valorRe = false,
+                    this.formRe = 'oculto',
+                    this.CssButton = '',
+
+                    this.valorFo = false,
+                    this.formFo = 'oculto',
+                    this.CssButtonFo = ''
+               }
+            },
+            clickButtonFo(){
+               if (this.valorFo == false){
+                    this.valorFo = true,
+                    this.formFo = '',
+                    this.CssButtonFo = 'background: #008AE6; color: #fff',
+
+                    this.valorRe = false,
+                    this.formRe = 'oculto',
+                    this.CssButton = '',
+                    
+                    this.valorTo = false,
+                    this.formTo = 'oculto',
+                    this.CssButtonTo = ''
+               }
+            }
+            
+            
+        }
+    });
+    </script>
+</body>
+</html>
