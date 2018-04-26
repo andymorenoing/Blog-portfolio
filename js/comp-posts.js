689f54b41a572077e5f187e47fe840a045e246ca
Andy <andymoreno.ing@gmail.com>
2018-04-26 19:42:47 -0400

    Varios arreglos en la pagina del blog y la del articulo

--- js/comp-posts.js
+++ js/comp-posts.js
@@ -1,4 +1,136 @@
-Vue.component('postsvue', {
+Vue.component('postsre', {
+    template: `
+        <div>
+            <h3>Articulos Relacionados</h3>
+            <div class="article-rating ed-container">
+                <div class="share" title="Compartir">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <div class="ed-item l-40 images-rating cross-center main-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="rating-star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="ed-item l-60">
+                    <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                    <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                    <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                </div>
+            </div>
+            <div class="article-rating ed-container">
+                <div class="share" title="Compartir">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <div class="ed-item l-40 images-rating cross-center main-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="rating-star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="ed-item l-60">
+                    <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                    <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                    <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                </div>
+            </div>
+            <div class="article-rating ed-container">
+                <div class="share" title="Compartir">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <div class="ed-item l-40 images-rating cross-center main-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="rating-star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="ed-item l-60">
+                    <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                    <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                    <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                </div>
+            </div>
+        </div>
+    `,
+    data () {
+        return {
+            
+        }
+    }
+});
+Vue.component('poststop', {
+    template: `
+        <div>
+            <h3>Articulos top 6!</h3>
+            <div class="article-rating ed-container">
+                <div class="share" title="Compartir">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <div class="ed-item l-40 images-rating cross-center main-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="rating-star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="ed-item l-60">
+                    <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                    <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                    <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                </div>
+            </div>
+            <div class="article-rating ed-container">
+                <div class="share" title="Compartir">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <div class="ed-item l-40 images-rating cross-center main-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="rating-star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="ed-item l-60">
+                    <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                    <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                    <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                </div>
+            </div>
+            <div class="article-rating ed-container">
+                <div class="share" title="Compartir">
+                    <i class="fas fa-share-square"></i>
+                </div>
+                <div class="ed-item l-40 images-rating cross-center main-center">
+                    <img src="img/blog.jpg" alt="">
+                    <div class="rating-star">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="ed-item l-60">
+                    <h4 class="artcile-rating-title"><a href="#">Titulo del article</a></h4>
+                    <p class="article-rating-p"><i class="far fa-comments"></i>3 comments</p>
+                    <p class="article-rating-p"><i class="fas fa-calendar"></i>02 marzo 2018</p>
+                </div>
+            </div>
+        </div>
+    `,
+    data () {
+        return {
+            
+        }
+    }
+});
+Vue.component('postsfiltro', {
     template: `
         <div>
             
